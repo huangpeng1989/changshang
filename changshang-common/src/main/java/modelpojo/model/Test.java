ed2027d0e52f7004d552871bcbac55d3ae02b5c8
huangp <huangp@galasys.cn>
2021-09-16 15:07:30 +0800

    33333

--- changshang-common/src/main/java/modelpojo/model/Test.java
+++
@@ -1,7 +0,0 @@
-package modelpojo.model;
-
-/**
- * Created by xietian on 2019/8/22.
- */
-public class Test {
-}
--- changshang-common/src/main/java/modelpojo/vo/Test.java
+++
@@ -1,7 +0,0 @@
-package modelpojo.vo;
-
-/**
- * Created by xietian on 2019/8/22.
- */
-public class Test {
-}
